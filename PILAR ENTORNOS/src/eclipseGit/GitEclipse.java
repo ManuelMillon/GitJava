854c97f5095186a0eb1c24f7d41e71ccace07fd3
manol <manol@192.168.1.131>
2021-02-13 21:24:06 +0100

    Nuevo añadido.

--- PILAR ENTORNOS/src/eclipseGit/GitEclipse.java
+++ PILAR ENTORNOS/src/eclipseGit/GitEclipse.java
@@ -14,6 +14,7 @@ public class GitEclipse {
 	 */
 	public static void main(String[] args) {
 		System.out.println("Hola Mundo");
+		System.out.println("Hola, Pilar. Espero que no me suspendas por el XML");
 	}
 
 }
