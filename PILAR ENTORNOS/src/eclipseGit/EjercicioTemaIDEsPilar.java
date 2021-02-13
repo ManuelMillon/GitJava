816428e75bcdfdfe4268736c76207ad9d5aa2b94
manol <manol@192.168.1.131>
2021-02-13 19:04:21 +0100

    Descripción por convencion.

---
+++ PILAR ENTORNOS/src/eclipseGit/EjercicioTemaIDEsPilar.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package eclipseGit;
+
+/**
+ * @author manol
+ *
+ */
+public class EjercicioTemaIDEsPilar {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ PILAR ENTORNOS/src/eclipseGit/GitEclipse.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package eclipseGit;
+
+/**
+ * @author manol
+ *
+ */
+public class GitEclipse {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hola Mundo");
+	}
+
+}
